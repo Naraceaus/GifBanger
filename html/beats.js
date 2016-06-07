f2776d98b6821a27b196f3b2e706c2dbb76ba7cc
Naraceaus <calin.arthy@gmail.com>
2016-06-07 21:51:11 +1000

    The Beginning
    Time to start using Git

---
+++ html/beats.js
@@ -0,0 +1,355 @@
+  //submit form on uploading image
+  function uploadImage() {
+   $("#imageform").submit()
+  }
+  
+  $("#fileToUpload").on("change", uploadImage);
+  
+  //beat measurer object
+   function beatHolder(tapSel, bpmSel, accBarSel, slideSel, mockGifSel) {
+    that=this;
+    this.tapSel = tapSel;
+    this.bpmSel = bpmSel;
+    this.accBarSel = accBarSel;
+    this.slideSel=slideSel;
+    this.mockGifSel = mockGifSel;
+    
+    //max siz of beats array
+    this.maxBeats = 4;
+    // timestamp of each beat
+    this.beats = [];
+    //contains BPM of lessening accuracy
+    this.accArray = [];
+    //min/max BPM based on history
+    this.minBPM = 0;
+    this.maxBPM = 0;
+    this.bpMs = 0;
+    this.startTime=0;
+    
+    //number of nothces in craphical metronome
+    this.metNotchNum = 20;
+    this.metMaxBPM  = 300;
+    this.timerActive = false;
+    
+    //selectr for beat timer bar
+    this.beatTimerSel = "beatTimer";
+    
+    //session id stored in page hidden input for image selection
+    this.sessionID = $('#sessionID').val();
+    
+    //oomphFrame is the frames of the split gif where the beat should land on
+    this.oomphFrames=[];
+    this.oomphGaps=[];
+    
+    this.frames=[];
+    
+    for (var findex = 1; findex<=$("."+slideSel).length; findex++) {
+     this.frames.push(findex);
+    }
+    //this.noFrames = $("."+slideSel).length;
+    
+    this.addBeat = function() {
+     var timeTapped=Date.now();
+     
+     //add the beat
+     that.beats.push(timeTapped);
+     
+     //remove earliest beat if too large
+     if (that.maxBeats > 0) {
+      if (that.beats.length>that.maxBeats) {
+       that.beats.shift();
+      }
+     }
+     
+     //removeprevious beats if its been too long since last click
+     if (that.beats[that.beats.length-1]-that.beats[that.beats.length-2]>(that.beats[1]-that.beats[0])*1.2) {
+      that.beats.splice(0,that.beats.length-1);
+     }
+     
+     // print out bpm if thecontainer exists in the dom
+     if ($('#'+that.bpmSel).length > 0) {
+      var bpmNow = that.calcBPM();
+      $('#'+that.bpmSel).text(bpmNow);
+      
+      //start playing frames
+      that.showCurrentFrame();
+      
+      that.setMetronome(bpmNow);
+     }
+     
+     // update beat accuracy bar
+      $("#"+that.accBarSel).text(that.calcAccuracy());
+      $("#minBPM").text(that.minBPM);
+      $("#maxBPM").text(that.maxBPM);
+      
+     //start the beat time if not already running, if already running reset
+     if (!that.timerActive && that.beats.length > 1) {
+      that.updateBeatTimer();
+     }
+     
+    }
+  
+    // calculate beats per min for given range of beats in array, no parameters assumes entire array
+    this.calcBPM = function(start, finish) {
+     if (start < 0 || start == null) {
+      start=0;
+     }
+    
+     if (finish == null) {
+      finish=that.beats.length-1;
+     }
+     
+     if (finish < 0) {
+      finish=0;
+     }
+     if (finish >= that.beats.length) {
+      finish = that.beats.length-1;
+     }
+     if (start > finish) {
+      start = finish;
+     }
+     
+     var numBeats = finish-start;
+     var beatRange = that.beats[finish]-that.beats[start];
+     var bpMs = (numBeats)/beatRange;
+     that.bpMs = bpMs;
+     var bPMin = bpMs*60000;
+     return bPMin;
+    }
+    
+    // create an array of beats per minue over time an set the over accuracy
+    // runs fewest (most recent) beats used to all used
+    this.calcAccuracy = function() {
+     that.accArray = [];
+     for (i = that.beats.length-2; i >0; i--) { 
+      that.accArray.push(Math.round(that.calcBPM(i,that.beats.length-1)));
+     }
+     
+     that.maxBPM = Math.max.apply(null, that.accArray);
+     that.minBPM = Math.min.apply(null, that.accArray);
+     
+     return that.accArray;
+    }
+    
+    this.hitSetOomphFrame = function(event){
+     var slideID = event.target.id;
+     var frameNumber = $("#"+slideID).data("count");
+     
+     that.setOomphFrame(frameNumber);
+    }
+    
+    //set the oomph beat frame takesan event from click
+    this.setOomphFrame = function (frameNumber) {
+     
+     var existingLocation = $.inArray(frameNumber,that.oomphFrames);
+     
+     if (existingLocation!=-1) {
+      that.oomphFrames.splice(existingLocation,1);
+     } else {
+      that.oomphFrames.push(frameNumber);
+      that.addFrame(frameNumber);
+     }
+     that.oomphFrames.sort(sortNumber);
+          
+     $("."+slideSel).removeClass("selected");
+          
+     for (var frameIter=0; frameIter<that.oomphFrames.length; frameIter++) {
+      $("#"+slideSel+that.oomphFrames[frameIter]).parent().addClass("selected");
+     }
+     
+    }
+    
+    this.showCurrentFrame = function() {
+     if (that.beats.length > 1 && that.oomphFrames.length>0) {
+      var curTime = Date.now();
+      var timeElapsed = curTime - that.beats[that.beats.length-1];
+      
+      var noBeats = timeElapsed * that.bpMs;
+      var noWholeBeats = Math.floor(noBeats);
+      
+      var beatFraction = noBeats-noWholeBeats;
+      
+      var oomphIndex = 0;
+      var framToShow = 0;
+      if (that.oomphFrames.length > 0) {
+       var oomphIndex = (noWholeBeats-1) % that.oomphFrames.length;
+       
+       var nextOomphIndex = (oomphIndex+1) % that.oomphFrames.length;
+       
+       var wholeOomph = that.oomphFrames[oomphIndex];
+       
+       var nextWholeOomph = that.oomphFrames[nextOomphIndex];
+       
+       if (nextWholeOomph > wholeOomph) {
+        
+        //remeber we need to count the number of activeframes not just simple subtraction
+        //var oomphLength = nextWholeOomph - wholeOomph;
+        var oomphLength = $.inArray(nextWholeOomph,that.frames)-$.inArray(wholeOomph,that.frames);
+        
+       } else {
+        var oomphLength = (that.frames.length-$.inArray(wholeOomph,that.frames))+$.inArray(nextWholeOomph,that.frames);
+       }
+       
+       var oomphFrameProgression = Math.floor(oomphLength * beatFraction);
+       var frameToShow = that.frames[(($.inArray(wholeOomph,that.frames) + oomphFrameProgression) % that.frames.length)];
+      }
+      
+      $("#"+that.mockGifSel).attr("src","uploads/"+that.sessionID+"_"+frameToShow+".jpeg");
+      
+      setTimeout(function() {that.showCurrentFrame();},10);
+     }
+    }
+    
+    this.hitAddFrame = function(event){
+     var addFrameID = event.target.id;
+     var frameNum = $("#"+addFrameID).data("count");
+     
+     that.addFrame(frameNum);
+     
+     $("#removeFrame"+frameNum).show();
+     $("#"+addFrameID).hide();
+    }
+    
+    this.addFrame = function (frameNum) {
+     
+     var existingLocation = $.inArray(frameNum,that.frames);
+     
+     if (existingLocation==-1) {
+      that.frames.push(frameNum);
+     }
+     that.frames.sort(sortNumber);
+     
+     $("#removeFrame"+frameNum).show();
+     $("#addFrame"+frameNum).hide();
+    }
+    
+    this.hitRemoveFrame = function(event){
+     var removeFrameID = event.target.id;
+     var frameNum = $("#"+removeFrameID).data("count");
+     
+     that.removeFrame(frameNum);
+     
+     if ($.inArray(frameNum,that.oomphFrames)!=-1) {
+      that.setOomphFrame(frameNum);
+     }
+     
+    }
+    
+    this.removeFrame = function (frameNum) {
+
+     var existingLocation = $.inArray(frameNum,that.frames);
+     
+     if (existingLocation!=-1) {
+      that.frames.splice(existingLocation,1);
+     }
+     
+     $("#addFrame"+frameNum).show();
+     $("#removeFrame"+frameNum).hide();
+
+    }
+    
+    this.createMetronome = function (containerID){
+
+     var metCont = document.getElementById(containerID); //Get svg element
+     
+     var metSVG = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+     metSVG.setAttribute("id","metSVG");
+     metSVG.setAttribute("xmlns","http://www.w3.org/2000/svg");
+     metSVG.setAttribute("height","100%");
+     metSVG.setAttribute("viewBox","160 0 40 200");
+     
+     var metG = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+     metG.setAttribute("id","metG");
+     metG.setAttribute("style","transform-origin:50%;transition:transform 1s");
+     
+     var metCirc = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+     metCirc.setAttribute("id","metCirc");
+     metCirc.setAttribute("r","100");
+     metCirc.setAttribute("cx","100");
+     metCirc.setAttribute("cy","100");
+     metCirc.setAttribute("style","fill:#ff0000;stroke:#000000;");
+     
+     var needle = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+     needle.setAttribute("id","needle");
+     needle.setAttribute("d","M 100,100 195,100");
+     needle.setAttribute("style","stroke-width:1px;stroke:lime;");
+     
+     
+     metG.appendChild(metCirc);
+     metSVG.appendChild(metG);
+     metSVG.appendChild(needle);
+     
+     
+     var rotateDegrees = 0;
+     for (var intervalCounter = 0;intervalCounter<that.metMaxBPM;intervalCounter+=that.metMaxBPM/that.metNotchNum) {
+      
+      rotateDegrees = Math.round((intervalCounter/that.metMaxBPM)*360)*-1;
+      
+      
+      var newG = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+      newG.setAttribute("id","beat"+intervalCounter);
+      newG.setAttribute("transform-origin","100px 100px");
+      newG.setAttribute("style","transform:rotate("+rotateDegrees+"deg)");
+      newG.setAttribute("x","100");
+      newG.setAttribute("y","100");
+      
+      var newText = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+      newText.setAttribute("id","beatText"+intervalCounter);
+      newText.setAttribute("style","font-size:13.47368431px;font-family:sans-serif;");
+      
+      var newTSpan = document.createElementNS("http://www.w3.org/2000/svg", 'tspan');
+      newTSpan.setAttribute("id","beatTSpan"+intervalCounter);
+      newTSpan.setAttribute("x","175");
+      newTSpan.setAttribute("y","105");
+      newTSpan.textContent=Math.round(intervalCounter);
+      
+      newText.appendChild(newTSpan);
+      newG.appendChild(newText);
+      
+      metG.appendChild(newG);
+     }
+     
+     metCont.appendChild(metSVG);
+     
+    }
+    
+    this.setMetronome = function(bpm) {
+     var rotateDegrees = Math.round(bpm/(that.metMaxBPM)*360);
+     $("#metG").css("transform", "rotate("+rotateDegrees+"deg)");
+    }
+    
+    this.updateBeatTimer = function() {
+     
+     that.timerActive = true;
+     if (that.beats.length > 1) {
+      var beatPercentage = 100- ((Date.now() - that.beats[that.beats.length-1])/(that.beats[1]-that.beats[0]))*100;
+      console.log(beatPercentage);
+      console.log(Date.now() - that.beats[that.beats.length-1], that.beats[1]-that.beats[0]);
+      
+      if (0<beatPercentage) {
+       $("#"+that.beatTimerSel).css("width",beatPercentage+"%");
+       setTimeout(function () {that.updateBeatTimer()},50);
+      } else {
+       that.timerActive = false;
+       $("#"+that.beatTimerSel).css("width","0");
+      }
+     } else {
+      this.timerActive = false;
+     }
+    }
+    
+    $("#"+that.tapSel).on("mousedown",that.addBeat);
+     
+    $(".toggleOomph").on("click", that.hitSetOomphFrame);
+    $(".removeFrame").on("click", that.hitRemoveFrame);
+    $(".addFrame").on("click", that.hitAddFrame);
+    
+    that.createMetronome("metronomeHolder");
+   };
+   
+   function sortNumber(a,b) {
+    return a - b;
+   }
+   
+   var mainBH = new beatHolder("clickme", "beatsholder", "accbar", "slide","mockGifImage");
+   
---
+++ html/index.php
@@ -0,0 +1,190 @@
+<?php
+if(session_id() == '') {
+    session_start();
+}
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include "GifFrameExtractor.php";
+
+if (!empty($_FILES["fileToUpload"]["name"])) {
+$imageFileType = pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION);
+$target_dir = "uploads/";
+$target_file = $target_dir . session_id() ."_origUpload.".$imageFileType;
+$uploadOk = 1;
+//echo "this is my file type" . $imageFileType;
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        //echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        //echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+// Allow certain file formats
+if($imageFileType != "gif" ) {
+    //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+ if (is_dir($target_dir) && is_writable($target_dir)) {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+        echo $_FILES["fileToUpload"]["tmp_name"].' '.$target_file;
+    }
+ } else {
+  echo "uploads directory no writable";
+ }
+}
+}
+?>
+<html>
+ <head>
+  <script src="jquery-2.2.4.min.js"></script>
+  <!--
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  -->
+  <link rel="stylesheet" href="bootstrap-3.3.6-dist/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <script src="bootstrap-3.3.6-dist/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  
+  <link rel="stylesheet" type="text/css" href="style.css"/>
+
+  </head>
+ <body>
+  <?php echo "<input id='sessionID' type='hidden' value='".session_id()."'/>" ?>
+  
+  <div class="container">
+  
+  <!-- upload your image -->
+  <div class="row">
+     <div class="col-md-12">
+      <div><h1><span class="expander clickGlyph glyphicon" data-toggle="collapse" data-target="#imgUpload"></span>Step 1. Upload a GIF</h1></div>
+      <div id="imgUpload" class="collapse in">
+      <h3>Click on the below image to upload a new gif</h3>
+       <form id="imageform" action="index.php" method="post" enctype="multipart/form-data">
+        <div class="image-upload">
+         <label for="fileToUpload">
+         <?php  
+         $sessionID=session_id();
+         $origFile="uploads/".session_id()."_origUpload.gif";
+         if (file_exists($origFile)) {
+         } else {
+          $origFile = "uploads/default.gif";
+         }
+           echo "<image class='mainImage' src='$origFile'>";
+         ?>
+         </label>
+         <input id="fileToUpload" name="fileToUpload" type="file"/>
+        </div>
+       </div>
+      </form>
+     </div>
+  </div>
+  <!-- Split Frames Section -->
+  <div class="row">
+   <div class="col-md-12">
+    <div><h1><span class="expander clickGlyph glyphicon collapsed" data-toggle="collapse" data-target="#gifThumbs"></span>Step 2. Select Your Frames</h1></div>
+    <div id="gifThumbs" class="collapse">
+    <?php
+    if (file_exists($origFile)) {
+     if (GifFrameExtractor::isAnimatedGif($origFile)) { // check this is an animated GIF
+      
+      $gfe = new GifFrameExtractor();
+      $gfe->extract($origFile);
+
+      $count=1;
+      
+      foreach ($gfe->getFrames() as $frame) {
+       // The frame resource image var
+       $img = $frame['image'];
+       $duration = $frame['duration'];
+
+       $imgname = "uploads/" . session_id() . "_" . $count . ".jpeg";
+       
+       imagejpeg($img, $imgname);
+       
+       echo "
+       <div class='col-lg-2 col-md-4 col-sm-6'>
+        <div class='thumbnail slide'>
+         <img src='$imgname'>
+         <span id='addFrame$count' class='addFrame clickGlyph glyphicon glyphicon-plus' data-count='$count' style='display:none;'></span>
+         <span id ='removeFrame$count' class='removeFrame clickGlyph glyphicon glyphicon-minus' data-count='$count'></span>
+         <span id='slide$count' class='toggleOomph	clickGlyph glyphicon glyphicon-music' data-count='$count'></span>
+         <span class='pull-right'>$count</span>
+        </div>
+       </div>";
+       // The frame duration
+       
+       $count++;
+      }
+     }   
+    }
+    ?>
+    </div>
+   </div>
+  </div>
+  
+  <!--Player-->
+  <div class="row">
+   <div class="col-md-12">
+    <div><h1><span class="expander clickGlyph glyphicon collapsed" data-toggle="collapse" data-target="#gifPlayer"></span>Step 3. Tap to the beat</h1></div>
+    <div id="gifPlayer" class="row collapse">   
+     <div class="col-md-5">
+     
+     
+      <div class="row">
+       <div class="col-xs-10 nopadding">
+        <div id="clickme" class="btn-large btn-xlarge">
+         <span class="glyphicon glyphicon-music"></span><span>Tap To The Beat</span><span class="glyphicon glyphicon-music"></span>
+         <br>
+         <div class="progress">
+           <div id="beatTimer" div="beatTimerBar" class="progress-bar">
+             <span class="sr-only"></span>
+           </div>
+         </div>
+        </div>
+       </div>
+       <div class="col-xs-2 nopadding"/>
+        <div id="metronomeHolder" class="circleContainer">
+        </div>
+        
+        <div id="beatsholder" style="display:none;">None Yet</div>
+        <br>
+        <div id="minBPM" style="display:none;">None Yet</div>
+        <br>
+        <div id="maxBPM" style="display:none;">Non Yet</div>
+        <br>
+       </div>
+       
+       
+      </div>
+     </div>
+     <div class="col-md-7">
+        <div id="mockGif">
+         <image id='mockGifImage' src='upload.png'>
+        </div>  
+     </div>
+    </div>
+   </div>
+  </div>
+  
+ </div>
+ <hr>
+ </body>
+ <footer>
+  <script src="beats.js"></script>
+ </footer>
+</html>
